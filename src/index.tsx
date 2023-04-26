331519f14312eccedf74692278dce4f5d7d31ff2
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-04-26 08:11:17 +0530

    refactor: update hooks path in index file

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ export { default as Then } from './lib/components/Then/Then'
 export { default as For } from './lib/components/For/For'
 
 // hooks exports
-export { default as useOnMountEffect } from './lib/hooks/use-on-mount-effect'
-export { default as useSyncedRef } from './lib/hooks/use-synced-ref'
+export { default as useOnMountEffect } from './lib/hooks/use-on-mount-effect/use-on-mount-effect'
+export { default as useSyncedRef } from './lib/hooks/use-synced-ref/use-synced-ref'
