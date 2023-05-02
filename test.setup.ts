0e8721dcffc075f690a50274733bcbd177f68d37
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-05-02 22:11:42 +0530

    [test]: update test-setup config for vitest
    - Add restoreMock

--- test.setup.ts
+++ test.setup.ts
@@ -1,4 +1,4 @@
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
 
@@ -6,4 +6,5 @@ expect.extend(matchers);
 
 afterEach(() => {
    cleanup();
+   vi.restoreAllMocks()
 });
