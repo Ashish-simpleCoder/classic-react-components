37d094488964a9750eeb8202844444c925fb88dc
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-06-29 21:33:56 +0530

    Remove hooks

--- src/lib/hooks/use-on-mount-effect/use-on-mount-effect.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useEffect } from 'react'
-
-export default function useOnMountEffect(cb: () => void | (() => void)) {
-   useEffect(cb, [])
-}
--- src/lib/hooks/use-synced-ref/use-synced-ref.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from 'react'
-
-export default function useSyncedRef<T>(state: T) {
-   const stateRef = useRef(state)
-
-   stateRef.current = state
-
-   return stateRef
-}
