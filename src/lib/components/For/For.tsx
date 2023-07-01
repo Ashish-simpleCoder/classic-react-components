a4b2e47361c8bf1985b076c5bc30edd53a92326b
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-07-01 18:59:01 +0530

    [feat]: update logic for For.tsx component for rendering

--- src/lib/components/For/For.tsx
+++ src/lib/components/For/For.tsx
@@ -1,30 +1,21 @@
 import React from 'react'
 
-export default function For({
-   loop = 0,
+export default function For<T extends any[]>({
+   data,
    children = null,
 }: {
-   children?: ((i: number) => JSX.Element) | null
-   loop?: number
+   children?: ((item: T[number], i: number) => JSX.Element) | null
+   data?: T
 }) {
-   if (children !== null && typeof children != 'function') {
+   if (!data || children === null) return <></>
+
+   if (typeof children != 'function') {
       throw new Error('Children type must be a function.')
    }
-   if (Number.isNaN(Number(loop))) {
-      throw new Error('Invalid value provided for the loop prop.')
-   }
 
-   if (children === null) {
-      return <></>
-   }
-
-   return <Elements item={children} loop={loop} />
-}
-
-function Elements({ item, loop }: { item: (i: number) => JSX.Element; loop: number }) {
    let arr: JSX.Element[] = []
-   for (let i = 0; i < Number(loop); i++) {
-      const element = item(i)
+   for (let i = 0; i < Number(data.length); i++) {
+      const element = children(data[i], i)
       arr.push(element)
    }
 
